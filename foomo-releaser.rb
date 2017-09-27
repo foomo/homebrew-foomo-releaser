a924dccef163fa1d7bdb5b2bfdcd6b62d00b3ce7
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-27 10:37:24 +0200

    foomo-releaser version 0.2

--- foomo-releaser.rb
+++ foomo-releaser.rb
@@ -1,9 +1,9 @@
 class FoomoReleaser < Formula
   desc "CLI Utility to generate releases for foomo-related projects"
   homepage "https://github.com/foomo/foomo-releaser"
-  url "https://github.com/foomo/foomo-releaser/releases/download/0.1/foomo-releaser_0.1_darwin_amd64.tar.gz"
-  version "0.1"
-  sha256 "9ecda15412a73a1db7d08a2067db1c55d0fda4c75e0a4c9bb26ca60ca99b136d"
+  url "https://github.com/foomo/foomo-releaser/releases/download/0.2/foomo-releaser_0.2_darwin_amd64.tar.gz"
+  version "0.2"
+  sha256 "dcaf93be43dedb580230ff3eb79dcc53671d933406493a083f0a3399747c8ecd"
 
   def install
     bin.install "foomo-releaser"
@@ -11,5 +11,9 @@ class FoomoReleaser < Formula
 
   def caveats
     "foomo-releaser -t {TOKEN} -d {DIRECTORY} -v {VERSION}"
+  end
+
+  test do
+    
   end
 end
